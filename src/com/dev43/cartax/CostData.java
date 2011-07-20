211296eb59c94cc590de0af472b925f1768534a4
Joakim Stoor <joakim.stoor@gmail.com>
2011-07-20 22:13:42 +0300

    meddelande

--- src/com/dev43/cartax/CostData.java
+++ src/com/dev43/cartax/CostData.java
@@ -1,21 +1,49 @@
 package com.dev43.cartax;
 
+import java.lang.reflect.Array;
+
 public class CostData {
 	public static String[] DEMO_AXES_LABELS = { "1000 km", "Cost" };
 	public static String DEMO_CHART_TITLE = "Car anual cost/km";
 
+	public static double dieselPrice=1.378;
+	public static double petrolPrice=1.587;
+
+	public static double[] DEMO_X_AXIS_DATA = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
 
 
-	public static double[] DEMO_X_AXIS_DATA = { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 };
+	public static String[] DEMO_TITLES = { "car1", "car2", "car3", "car4" };
 
-
-	public static String[] DEMO_TITLES = { "Car1", "Car2", "Car3", "Car4" };
-
-
-	public static double[] DEMO_SERIES_1 = { 12.3, 12.5, 13.8, 16.8, 20.4, 24.4, 26.4, 26.1, 23.6, 20.3 };
-	public static double[] DEMO_SERIES_2 = { 10, 10, 12, 15, 20, 24, 26, 26, 23, 18 };
-	public static double[] DEMO_SERIES_3 = { 5, 5.3, 8, 12, 17, 22, 24.2, 24, 19, 15 };
-	public static double[] DEMO_SERIES_4 = { 9, 10, 11, 15, 19, 23, 26, 25, 22, 18 };   
-
+	public static double[] DEMO_SERIES_1 = calculateDrivingCost(true, 2005, 1401, 118, 5.2/100);
+	public static double[] DEMO_SERIES_2 = calculateDrivingCost(true, 2004, 1320, 134, 7.0/100);
+	public static double[] DEMO_SERIES_3 = calculateDrivingCost(false, 2004, 1399, 140, 8.3/100);
+	public static double[] DEMO_SERIES_4 = calculateDrivingCost(false, 2010, 1500, 130, 5.2/100);   
+	
 	public static double[][] DEMO_SERIES_LIST = {DEMO_SERIES_1, DEMO_SERIES_2, DEMO_SERIES_3, DEMO_SERIES_4};
+	
+	public static double[] calculateDrivingCost(boolean isDiesel,int year, int weight, int co2, double lpkm)
+	{
+		double startCost=0;
+		double additionalCost=0;
+		double[] cost = new double[10];
+		double kmPrice=0;
+		
+		if(isDiesel)
+		{
+			startCost=70;
+			additionalCost=Math.ceil(weight/100)*0.055*365;
+			kmPrice=dieselPrice*lpkm;
+			
+		}
+		else
+		{
+			startCost=70;
+			kmPrice=petrolPrice*lpkm;
+		}
+		for (int i=0; i<10; i++){
+			cost[i]=startCost+additionalCost+(i+1)*2000*kmPrice;
+		}
+		
+		return cost;
+	}
 }
